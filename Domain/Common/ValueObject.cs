3cd87e1feb5fedd90fa926ad5e8e5389b4b37735
Ahmet Abdullah <realahmetabdullah@gmail.com>
2026-04-23 13:08:47 +0300

    Added a new Domain project targeting .NET 8.0 with MediatR.Contracts dependency. Added foundational domain classes: BaseEntity , BaseAuditableEntity, BaseEvent, and ValueObject.

---
+++ Domain/Common/ValueObject.cs
@@ -0,0 +1,57 @@
+﻿using System.Buffers.Text;
+
+namespace Domain.Common;
+//This is a base class for DDD Value Objects that enforces value-based equality by comparing all defined components instead of references,
+//and it also overrides Equals, GetHashCode, and ==/!= operators so that two objects are considered equal only if their meaningful fields match exactly.
+public abstract class ValueObject
+{
+    protected static bool EqualOperator(ValueObject left, ValueObject right)
+    {
+        if (left is null ^ right is null)
+        {
+            return false;
+        }
+
+        return left?.Equals(right!) != false;
+    }
+
+    protected static bool NotEqualOperator(ValueObject left, ValueObject right)
+    {
+        return !(EqualOperator(left, right));
+    }
+
+    protected abstract IEnumerable<object> GetEqualityComponents();
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || obj.GetType() != GetType())
+        {
+            return false;
+        }
+
+        var other = (ValueObject)obj;
+        return GetEqualityComponents().SequenceEqual(other.GetEqualityComponents());
+    }
+
+    public override int GetHashCode()
+    {
+        var hash = new HashCode();
+
+        foreach (var component in GetEqualityComponents())
+        {
+            hash.Add(component);
+        }
+
+        return hash.ToHashCode();
+    }
+
+    public static bool operator ==(ValueObject left, ValueObject right)
+    {
+        return EqualOperator(left, right);
+    }
+
+    public static bool operator !=(ValueObject left, ValueObject right)
+    {
+        return NotEqualOperator(left, right);
+    }
+}
